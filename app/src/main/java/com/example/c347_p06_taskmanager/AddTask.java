aca12d57613c749dd8526db24fda78efc7aae6c4
HoJiaDe <17010621@myrp.edu.sg>
2019-05-30 15:16:44 +0800

    bth

--- app/src/main/java/com/example/c347_p06_taskmanager/AddTask.java
+++ app/src/main/java/com/example/c347_p06_taskmanager/AddTask.java
@@ -2,8 +2,13 @@ package com.example.c347_p06_taskmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AddTask extends AppCompatActivity {
+
+
+    EditText etName, etDescription;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/c347_p06_taskmanager/NotificationReceiver.java
@@ -0,0 +1,63 @@
+package com.example.c347_p06_taskmanager;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+public class NotificationReceiver extends BroadcastReceiver {
+
+    int reqCode = 12345;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(NOTIFICATION_SERVICE);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("default", "Default Channel", NotificationManager.IMPORTANCE_DEFAULT);
+
+            channel.setDescription("This is for default notification");
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Intent i = new Intent(context, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(context, reqCode, i, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
+        builder.setContentTitle("Task Manager Reminder");
+
+        String name = intent.getStringExtra("name");
+        builder.setContentText(name);
+
+        Log.e("name", name);
+
+        builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+        builder.setContentIntent(pIntent);
+        builder.setAutoCancel(true);
+
+        builder.setLights(Color.WHITE, 500, 500);
+
+        long[] pattern = {500,500,500,500,500,500,500,500,500};
+        builder.setVibrate(pattern);
+
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        builder.setSound(alarmSound);
+
+        Notification n = builder.build();
+
+        notificationManager.notify(123, n);
+
+    }
+}
